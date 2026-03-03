f8e6affbd327d1c92327c85cc56749504b93a594
Melroy Barreto <melroy.barreto@siemens.com>
2026-03-03 16:47:11 +0530

    Enhance BookDetails component with improved layout and additional metadata display

--- client/src/pages/BookDetails.jsx
+++ client/src/pages/BookDetails.jsx
@@ -1,10 +1,13 @@
 import { useEffect, useState, useRef } from "react";
 import { useParams } from "react-router-dom";
 import useBookDetail from "../hooks/useBookDetail";
+// MUI components
 import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
 const BookDetails = () => {
   const { bookId, authorId } = useParams();
@@ -15,10 +18,18 @@ const BookDetails = () => {
   //Custom hooks
   const bookDetail = useBookDetail(bookId, authorId);
   console.log("Book Detail:", bookDetail);
-  const description = bookDetail?.description;
-  const title = bookDetail?.title ?? "";
-  const personal_name = bookDetail?.author?.personal_name ?? "Unknown Author";
-  const covers = bookDetail?.covers ?? [];
+
+  const {
+    description = "No description available",
+    title = "Untitled",
+    author: { personal_name = "Unknown Author" } = {},
+    covers: rawCovers = [],
+    revision = 0,
+  } = bookDetail || {};
+  // const description = bookDetail?.description;
+  // const title = bookDetail?.title ?? "";
+  // const personal_name = bookDetail?.author?.personal_name ?? "Unknown Author";
+  // const covers = bookDetail?.covers ?? [];
 
   const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: "#fff",
@@ -48,10 +59,13 @@ const BookDetails = () => {
 
   return (
     <Box sx={{ flexGrow: 1, padding: "20px" }}>
-      <Grid container className="rounded text-body-emphasis">
-        <Grid className="p-3" size={{ lg: 9 }}>
+      <Grid container className="rounded text-body-emphasis" spacing={1}>
+        <Grid className="p-3 border rounded flex-column" size={{ lg: 9 }}>
           <h1 className="display-6 fst-italic">{title}</h1>{" "}
-          <p>by {personal_name}</p>
+          <p>
+            by {personal_name}
+            <span style={{ fontWeight: "lighter" }}> ({revision})</span>
+          </p>
           <p
             ref={descriptionRef}
             className={`book-description my-3 ${isExpanded ? "book-description--expanded" : ""}`}
@@ -63,21 +77,35 @@ const BookDetails = () => {
               className="read-more-btn"
               onClick={() => setIsExpanded((prev) => !prev)}
             >
-              {isExpanded ? "Show Less" : "Show More"}
+              {isExpanded ? (
+                <>
+                  Read Less <KeyboardArrowUpIcon />
+                </>
+              ) : (
+                <>
+                  Read More <KeyboardArrowDownIcon />
+                </>
+              )}
             </button>
           )}
+          <Box className="book-meta__container">
+            <Box className="book-meta__item">Publish Date</Box>
+            <Box className="book-meta__item">Publisher</Box>
+            <Box className="book-meta__item">Language</Box>
+            <Box className="book-meta__item">Pages</Box>
+          </Box>
         </Grid>
 
         <Grid
-          className="p-3 d-flex justify-content-center align-items-center"
+          className="p-1 d-flex justify-content-center align-items-start border rounded"
           size={{ lg: 3 }}
         >
           <Box className="cover-image__container">
             <img
               className="cover-image__item"
               src={
-                covers?.length > 0
-                  ? `https://covers.openlibrary.org/b/id/${covers[0]}-M.jpg`
+                rawCovers?.length > 0
+                  ? `https://covers.openlibrary.org/b/id/${rawCovers[0]}-M.jpg`
                   : "https://dummyimage.com/150x200/cccccc/000000&text=No+Cover"
               }
               alt="book-cover-main"
